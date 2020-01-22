186bccbe284d28055860bdb75b9040a958888d12
mdurrant <mdurrant@expressmedia.com>
2020-01-22 02:05:43 -0700

    more cleanup, better watch

--- gulpfile.js
+++ gulpfile.js
@@ -2,6 +2,8 @@ const fileinclude = require('gulp-file-include');
 const gulp = require('gulp');
 const del = require('del');
 const replace = require('gulp-token-replace');
+const webserver = require('gulp-webserver');
+const watch = require('gulp-watch');
 
 const clean = () => {
     return del(['./dist/**']);
@@ -34,4 +36,21 @@ const misc = () => {
     return gulp.src(['./src/CNAME']).pipe(gulp.dest('./dist'));
 };
 
+const serve = () => {
+    return gulp.src('dist').pipe(
+        webserver({
+            livereload: true,
+            port: 4209
+        })
+    );
+};
+
 gulp.task('default', gulp.series(clean, html, assets, misc));
+
+gulp.task(
+    'watch',
+    gulp.series(html, serve, () => {
+        gulp.watch('./src/**/*.html', html);
+        gulp.watch('./src/assets/**/*', assets);
+    })
+);
