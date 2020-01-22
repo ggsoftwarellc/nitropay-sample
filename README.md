1e73ac7c293cdf78500f8caca85ec9773d551889
mdurrant <mdurrant@expressmedia.com>
2020-01-22 01:32:05 -0700

    ensure CNAME is copied

---
+++ README.md
@@ -0,0 +1,15 @@
+# Dev
+
+`yarn start`
+
+Go to
+
+`localhost:4209`
+
+# Building
+
+`yarn build`
+
+# Publishing
+
+`` git push origin `git subtree split --prefix dist master`:gh-pages --force ``
--- gulpfile.js
+++ gulpfile.js
@@ -22,4 +22,8 @@ const assets = () => {
     return gulp.src(['./src/assets/**/*']).pipe(gulp.dest('./dist/assets'));
 };
 
-gulp.task('default', gulp.series(clean, html, assets));
+const misc = () => {
+    return gulp.src(['./src/CNAME']).pipe(gulp.dest('./dist'));
+};
+
+gulp.task('default', gulp.series(clean, html, assets, misc));
